5706a58f0f5850b66c7045a45b073b496b35eaef
Jessica Vargas <139159958+javar10@users.noreply.github.com>
2025-10-13 22:23:13 -0400

    initial commit

--- App.tsx
+++ App.tsx
@@ -1,10 +1,33 @@
 import { StatusBar } from 'expo-status-bar';
+import { useState, useEffect } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import * as Font from 'expo-font';
+import { colors, spacing, fonts } from './styles/theme';
+
 
 export default function App() {
+  const [fontsLoaded, setFontsLoaded] = useState(false);
+
+  useEffect(() => {
+    async function loadFonts() {
+      await Font.loadAsync({
+        'spaceMono-regular': require('./assets/fonts/SpaceMono-Regular.ttf'),
+        'spaceMono-bold': require('./assets/fonts/SpaceMono-Bold.ttf'),
+      });
+      setFontsLoaded(true);
+    }
+
+    loadFonts();
+  }, []);
+
+  if (!fontsLoaded) {
+    // Render nothing or a custom splash screen while fonts load
+    return null;
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text style={styles.heading}>Open up App.tsx to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +36,12 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: colors.neutral500,
+    padding: spacing.lg,
+  },
+  heading: {
+    color: colors.neutral900,
+    fontSize: fonts.size.xxs,
+    fontFamily: fonts.bold,
   },
 });
---
+++ styles/theme.js
@@ -0,0 +1,34 @@
+export const colors = {
+    neutral900: '#141D2F',
+    neutral800: '#1E2A47',
+    neutral700: '#2B3442',
+    neutral500: '#4B6A9B',
+    neutral300: '#697C9A',
+    neutral200: '#90A4D4',
+    neutral100: '#F6F8FF',
+    neutral0: '#FFFFFF',
+    blue500: '#0079FF',
+    blue300: '#60ABFF',
+    red500: '#F74646',
+};
+
+export const spacing = {
+    xs: 4,
+    sm: 8,
+    md: 16,
+    lg: 24,
+    xl: 32,
+};
+
+export const fonts = {
+    regular: 'spaceMono-regular',
+    bold: 'spaceMono-bold',
+    size: {
+        xl: 26,
+        lg: 22,
+        md: 18,
+        sm: 16,
+        xs: 15,
+        xxs: 13,
+    },
+};
